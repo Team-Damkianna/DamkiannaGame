ba71e8f167a339bd89ea3d196f8fb69936c2a9ba
anichka777 <avalcheva7@gmail.com>
2014-12-13 00:46:08 +0200

    packages

---
+++ SurvivalShip/Assets/Scripts/SphereScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereScript : MonoBehaviour {
+
+    public float floatSpeed = .5F;
+    public float floatTime = 0F;
+    public float floatDistance = .01F;
+    public float rotateSpeed = 1F;
+    public int spheresCollected = 0;
+    public GameObject sphere;
+
+    // Use this for initialization
+    void Start()
+    {
+        Screen.showCursor = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        floatTime += Time.deltaTime + floatSpeed;
+        transform.Translate(0F, Mathf.Sin(Mathf.PI * 2 * floatTime) * floatDistance, 0F);
+
+        transform.Rotate(0F, rotateSpeed, 0F);
+    }
+   
+    void OnTriggerEnter(Collider other)
+    {     
+        Destroy(this.transform.gameObject);
+        SpheresCollected();
+
+    }
+    void SpheresCollected() {
+        spheresCollected++;
+        //Debug.Log(spheresCollected);
+    }
+
+}
---
+++ SurvivalShip/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerScript : MonoBehaviour {
+    float timeRemaining = 10;
+    public GUIStyle style;
+	// Use this for initialization
+	void Start () {
+        style = new GUIStyle();
+        style.fontSize = 30;
+        style.normal.textColor = Color.yellow;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeRemaining -= Time.deltaTime;
+	}
+    void OnGUI() {
+        if (timeRemaining > 0)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 80, 200, 100), "Time remaining: " + (int)timeRemaining, style);
+        }
+        else {
+            GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2, 200, 100), "Time's up! Sorry!", style);
+            //Application.LoadLevel("GameOverScene"); 
+        }
+    }
+}
